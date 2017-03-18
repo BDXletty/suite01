dc68cb7fdea550b6aae2653bad993d52f6be4c21
BDXletty <laetitiadervaux33@gmail.com>
2017-03-18 18:48:40 +0100

    EX07 done

--- ex07.js
+++ ex07.js
@@ -22,3 +22,12 @@ boostedAddition([4, 5, 6]);
 boostedAddition([4, 4, 6, 8, 10, 12]);
 
 //  écrire votre code sous ce commentaire
+function boostedAddition (tabNombre) {
+    let sum = 0;
+    for (i=0; i<tabNombre.length; i++) {
+        sum = sum + tabNombre[i];
+    }
+    return sum;
+}
+console.log(boostedAddition([4, 5, 6]));
+console.log(boostedAddition([4, 4, 6, 8, 10, 12]));
